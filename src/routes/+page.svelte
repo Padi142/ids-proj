a1314fc99bd1af39b0abe0b9667b8ab4e0e52063
Padias <matyas.krejza@seznam.cz>
2024-05-01 02:57:06 +0200

    formating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,40 @@
 <script lang="ts">
-	import Check from "svelte-radix/Check.svelte";
-	import CaretSort from "svelte-radix/CaretSort.svelte";
-	import { tick } from "svelte";
-	import * as Command from "$lib/components/ui/command/index.js";
-	import * as Popover from "$lib/components/ui/popover/index.js";
-	import * as Card from "$lib/components/ui/card/index.js";
-	import { Button } from "$lib/components/ui/button/index.js";
-	import { cn } from "$lib/utils.js";
+	import Check from 'svelte-radix/Check.svelte';
+	import CaretSort from 'svelte-radix/CaretSort.svelte';
+	import { tick } from 'svelte';
+	import * as Command from '$lib/components/ui/command/index.js';
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { cn } from '$lib/utils.js';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
 	const vehicles = data.vehicles.map((vehicle) => ({
 		value: vehicle.id,
-		label: vehicle.model + " " + vehicle.license_plate,
+		label: vehicle.model + ' ' + vehicle.license_plate
 	}));
 
 	//filter items based on menu.vehicleItems where vehicle_id = selectedVehicle and map that to data.menu
-	$: items = data.vehicleItems.filter((item) => item.vehicle_id === selectedVehicle).map((item) => {
-		const it = data.menu.filter((it) => it.id === item.menu_item_id)[0];
-		return {
-			name:it.name,
-			description:it.description,
-			image:it.image,
-			price:it.price
-		};
-	});
+	$: items = data.vehicleItems
+		.filter((item) => item.vehicle_id === selectedVehicle)
+		.map((item) => {
+			const it = data.menu.filter((it) => it.id === item.menu_item_id)[0];
+			return {
+				name: it.name,
+				description: it.description,
+				image: it.image,
+				price: it.price
+			};
+		});
 
 	let open = false;
 
 	let selectedVehicle = 1;
 
 	$: selectedValue =
-		vehicles.find((f) => f.value === selectedVehicle)?.label ?? "Select a vehicle...";
+		vehicles.find((f) => f.value === selectedVehicle)?.label ?? 'Select a vehicle...';
 
 	// We want to refocus the trigger button when the user selects
 	// an item from the list so users can continue navigating the
@@ -46,57 +48,54 @@
 </script>
 
 <div
-	class="flex flex-col items-center justify-start bg-cover background-color background-image h-min-screen  pt-8 pb-20"
+	class="background-color background-image h-min-screen flex flex-col items-center justify-start bg-cover pb-20 pt-8"
 >
-	<div class="w-full flex flex-row items-center justify-between px-14">
+	<div class="flex w-full flex-row items-center justify-between px-14">
 		<div></div>
-		<a href="/admin">
-			Admin
-		</a>
+		<a href="/admin"> Admin </a>
 	</div>
-	<h1 class="text-4xl font-bold text-center text-slate-700">Restaurant Vehicle Menu</h1>
+	<h1 class="text-center text-4xl font-bold text-slate-700">Restaurant Vehicle Menu</h1>
 	<div class="m-8">
-
-	<Popover.Root bind:open let:ids >
-		<Popover.Trigger asChild let:builder>
-			<Button
-				builders={[builder]}
-				variant="outline"
-				role="combobox"
-				aria-expanded={open}
-				class="w-[600px] justify-between"
-			>
-				{selectedValue}
-				<CaretSort class="w-4 h-4 ml-2 opacity-50 shrink-0" />
-			</Button>
-		</Popover.Trigger>
-		<Popover.Content class="w-[600px] p-0">
-			<Command.Root>
-				<Command.Input placeholder="Search vehicle..." class="h-9" />
-				<Command.Empty>No vehicle found.</Command.Empty>
-				<Command.Group>
-					{#each vehicles as vehicle}
-						<Command.Item
-							value={vehicle.value.toString()}
-							onSelect={(currentValue) => {
-								console.log(currentValue);
-              selectedVehicle = +currentValue;
-              closeAndFocusTrigger(ids.trigger);
-            }}
-						>
-							<Check
-								class={cn(
-                "mr-2 h-4 w-4",
-                selectedValue !== vehicle.value.toString() && "text-transparent"
-              )}
-							/>
-							{vehicle.label}
-						</Command.Item>
-					{/each}
-				</Command.Group>
-			</Command.Root>
-		</Popover.Content>
-	</Popover.Root>
+		<Popover.Root bind:open let:ids>
+			<Popover.Trigger asChild let:builder>
+				<Button
+					builders={[builder]}
+					variant="outline"
+					role="combobox"
+					aria-expanded={open}
+					class="w-[600px] justify-between"
+				>
+					{selectedValue}
+					<CaretSort class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+				</Button>
+			</Popover.Trigger>
+			<Popover.Content class="w-[600px] p-0">
+				<Command.Root>
+					<Command.Input placeholder="Search vehicle..." class="h-9" />
+					<Command.Empty>No vehicle found.</Command.Empty>
+					<Command.Group>
+						{#each vehicles as vehicle}
+							<Command.Item
+								value={vehicle.value.toString()}
+								onSelect={(currentValue) => {
+									console.log(currentValue);
+									selectedVehicle = +currentValue;
+									closeAndFocusTrigger(ids.trigger);
+								}}
+							>
+								<Check
+									class={cn(
+										'mr-2 h-4 w-4',
+										selectedValue !== vehicle.value.toString() && 'text-transparent'
+									)}
+								/>
+								{vehicle.label}
+							</Command.Item>
+						{/each}
+					</Command.Group>
+				</Command.Root>
+			</Popover.Content>
+		</Popover.Root>
 	</div>
 
 	{#each items as item}
@@ -109,9 +108,9 @@
 					<img
 						src={item.image}
 						alt={item.image}
-						class="object-cover w-40 h-40 shadow-2xl rounded-xl"
+						class="h-40 w-40 rounded-xl object-cover shadow-2xl"
 					/>
-					<p class="p-4 w-full">
+					<p class="w-full p-4">
 						{item.description}
 					</p>
 					<p>
